7a313ec2ccc2c4ed5d16832d965f6eab2f99edd0
Brandon <dusk.argentum@gmail.com>
2015-09-15 21:48:32 -0400

    Burma Shave (Home)

---
+++ chapter1/BurmaShave/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurmaShave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maybe you can't");
+            Console.WriteLine("Shoulder a gun");
+            Console.WriteLine("But you can shoulder");
+            Console.WriteLine("The cost of one");
+            Console.WriteLine("Buy defense bonds");
+            Console.WriteLine("Burma Shave");
+        }
+    }
+}
