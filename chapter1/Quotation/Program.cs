399c55cfa0600a0809ec6997da3ed617a0e751e5
Brandon <dusk.argentum@gmail.com>
2015-09-29 11:30:13 -0400

    Quotation

---
+++ chapter1/Quotation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is our choices...that show what we truly are, far more than our abilities.");
+        }
+    }
+}
